dcb2695fd94462e2fed0aaf8e0c47da53879e01e
Tirumalanaik <tirunaik335@gmail.com>
2024-11-27 10:55:38 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import {Container} from "react-bootstrap";
+import Header from "./Components/Header";
 const App = () => {
   return (
+    <>
+    <header/>
+    <main ClassName="py-3">
+      <Container>
     <h1>
      Welcome To Ultra-shop 
     </h1>
+    </Container>
+    </main>
+    </>
   )
 }
 
-export default App
+export default App;
